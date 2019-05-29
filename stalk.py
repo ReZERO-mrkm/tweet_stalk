f271e226990bdf9a17e9700cca005fc808fbe0c6
Snufkin0866 <snufkin0866@gmail.com>
2019-05-29 22:21:53 +0900

    implement picture sending

--- stalk.py
+++ stalk.py
@@ -1,6 +1,7 @@
 from requests_oauthlib import OAuth1Session
 import json
 from urllib import request
+import urllib
 import config
 from time import sleep
 import requests
@@ -33,7 +34,8 @@ while True:
             "count": 1,
             "include_entities": True,
             "exclude_replies": False,
-            "include_rts": True
+            "include_rts": True,
+            "tweet_mode": "extended"
         }
         try:
             if firsts[i] == 0:
@@ -42,34 +44,52 @@ while True:
                 twi = timeline[0]
                 firsts[i] += 1
                 print(f'First Sending {twi["user"]["name"]} tweet.')
-                payload = {'content': twi['user']['name']+ 'のつぶやき：' + twi['text']}
+                payload = {'content': twi['user']['name']+ 'のつぶやき：' + twi['full_text']}
                 requests.post(discord_webhook_url, data=payload)
                 sleep(1)
                 payload = {'content': line}
                 requests.post(discord_webhook_url, data=payload)
                 firsts[i] += 1
+                if 'extended_entities' in twi.keys():
+                    image_path = "image/image.png"
+                    for e in twi['extended_entities']['media']:
+                        print(f'Sending picture of {i}')
+                        image_url = e['media_url']
+                        urllib.request.urlretrieve(image_url, image_path)
+                        payload = {'file': (image_path, open(
+                            image_path, 'rb'), 'image/png')}
+                        requests.post(discord_webhook_url, files=payload)
             elif firsts[i] == 2:
                 req = sess.get(TL, params=params1)
                 timeline = json.loads(req.text)
                 twi = timeline[0]
-                tmps[i] = twi['text']
+                tmps[i] = twi['full_text']
                 firsts[i] += 1
             elif firsts[i] == 3:
                 req = sess.get(TL, params=params1)
                 timeline = json.loads(req.text)
                 twi = timeline[0]
-                if (tmps[i] != twi['text']):
+                if (tmps[i] != twi['full_text']):
                     print(f'Sending {twi["user"]["name"]} tweet.')
-                    payload = {'content': twi['user']['name'] + 'のつぶやき：' + twi['text']}
+                    payload = {'content': twi['user']['name'] + 'のつぶやき：' + twi['full_text']}
                     requests.post(discord_webhook_url, data=payload)
                     sleep(1)
                     payload = {'content': line}
                     requests.post(discord_webhook_url, data=payload)
                     firsts[i] -= 1
+                    if 'extended_entities' in twi.keys():
+                        image_path = "image/image.png"
+                        for e in twi['extended_entities']['media']:
+                            print(f'Sending picture of {i}')
+                            image_url = e['media_url']
+                            urllib.request.urlretrieve(image_url, image_path)
+                            payload = {'file': (image_path, open(image_path, 'rb'), 'image/png')}
+                            requests.post(discord_webhook_url, files=payload)
+
+
         except KeyboardInterrupt:
             sys.exit()
         except:
             traceback.print_exc()
         finally:
             sleep(5)
-    
